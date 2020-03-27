6170f7ba603c18e5916578ef7a7bd060661149c4
Akhil Choudhary <38153141+akhilxyz@users.noreply.github.com>
2020-03-27 19:14:54 +0530

    first Commit

--- 4-Arrays/3-Looping.js
+++ 4-Arrays/3-Looping.js
@@ -1,7 +1,17 @@
 
+// forEach loop
+
 const Notes = ['Note 1','Note 2','Note 3' , 'Note 4']
 
 Notes.forEach(function (item , index) {
-    console.log(index)
-    console.log(item)
+    const num = index+1
+    console.log(`${index}. ${item}` )
 })
+
+// for Loop 
+
+for (let count = 0 ;count < Notes.length; count++ ){
+    const num = count + 1
+    const Note  = Notes[count]
+    console.log(`${num}. ${Note}`)
+}
